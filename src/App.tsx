fbf9f611ef4a77c2d9223d433fce0cff9938f683
Hayden Kirkeide <haydenkirkeide@gmail.com>
2026-08-31 17:12:22 -0400

    add OLD PROJECT

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ function App() {
     { name: "Lab 1 — D3 Bar Chart", url: "#" },
     { name: "Lab 2 — D3 Interactions", url: "#" },
     { name: "Final Project", url: "#" },
-    { name: "Test Project", url: "#" }
+    { name: "Test Project", url: "#" },
+    { name: "OLD HOMEPAGE", url: "#" }
   ];
 
   return (
