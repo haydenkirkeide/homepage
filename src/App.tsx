6a3e3ec96b64faada465cb3de162f28856b36431
Hayden Kirkeide <haydenkirkeide@gmail.com>
2026-09-04 16:17:26 -0400

    finished experience, started working on multiple pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import headshot from "./Hayden square.jpg";
 
 function App() {
   const projects = [
@@ -16,6 +17,7 @@ function App() {
       <header style={{ padding: "48px 24px" }}>
         <div className = "Section" > Homepage </div>
         <h1>Hayden Kirkeide</h1>
+        <img src={headshot} alt="Hayden Kirkeide" className="profile-photo" />
         <h2> About</h2>
         <p>Hayden Kirkeide is a multidisciplinary creative focusing on the intersection of art, 
           technology, and humanity through data science from and residing in Miami, Florida. 
@@ -29,47 +31,77 @@ function App() {
           Kirkeide plans on working as a data scientist following graduation and hopes to return to San 
           Diego sometime in the near future. </p>
 
+        <h2> Education </h2>
+        <h3> Florida State University </h3>
+        <p> Tallahassee, Florida | Expected Graduation Date: December 2026 </p>
+        <ul>
+          <li> M.S. Data Science: Computer Science</li>
+          <li> GPA: 3.9/4.0 </li>
+        </ul>
+        <h3> University of California, San Diego</h3>
+        <p> La Jolla, California | Graduation Date: June 2025 </p>
+        <ul>
+          <li> B.A. Communication </li>
+          <li> B.A. Interdisciplinary Computing and the Visual Arts </li>
+          <ul> <li> Applied Computer Science </li></ul>
+          <li> GPA: 3.7/4.0</li>
+        </ul>
+
         <h2> Experience </h2>
         <h3> Guest Experience Insights Analyst Intern </h3>
         <p> Carnival Cruise Line, Miami, Florida <br /> May 2026 - August 2026 </p>
       
         <ul> 
-          <li> PL1 </li> 
-          <li> PL2 </li> 
-          <li> PL3 </li> 
+          <li> Constructing real-time dashboard in Power BI to track performance of 10+ operational updates aboard Carnival Conquest. </li> 
+          <li> Recalculating Net Promoter Score-to-annual revenue ratio to enhance revenue forecasting through SQL and SAS querying. </li> 
+          <li> Implementing guest segmentation and clustering algorithm to pinpoint satisfaction gaps across Carnival’s 29 ships using SAS. </li> 
           </ul>
         <h3> Undergraduate Admissions Student Assistant </h3>
         <p> UC San Diego Office of Undergraduate Admissions, La Jolla, California <br /> December 2021 - December 2024 </p>
         <ul> 
-          <li> PL1 </li> 
-          <li> PL2 </li> 
-          <li> PL3 </li> 
+          <li> Analyzed frequency distribution and likelihood of guest communication in Python, resulting an optimized staffing schedule.</li> 
+          <li> Maintained running analytic dashboard of student records and documents in Slate, averaging around 30 incoming files a day. </li> 
+          <li> Coordinated daily shipments of admissions materials to Admissions Officers, overseeing delivery of over 36,000 brochures.
+ </li> 
           </ul>
         <h3> Technical Writing Intern </h3>
         <p> Hologic, Inc., San Diego, California <br /> June 2023 - August 2023 </p>
         <ul> 
-          <li> PL1 </li> 
-          <li> PL2 </li> 
-          <li> PL3 </li> 
+          <li> Overhauled global Technical Writer’s Handbook using HTML, XML, and CSS languages in single-source authoring software.
+ </li> 
+          <li> Streamlined Standard Operating Procedure workflows in Agile, enhancing global Technical Writing team efficiency by 8%.
+ </li> 
+          <li> Updated 3 life cycle process documentation management for diagnostic systems in single-source authoring software.
+ </li> 
           </ul>
         <h3> Digital and Consumer Marketing Intern </h3>
         <p> AutoNation, Inc., Fort Lauderdale, Florida </p>
         <ul> 
-          <li> PL1 </li> 
-          <li> PL2 </li> 
-          <li> PL3 </li> 
+          <li> Analyzed statistics of 320+ AutoNation franchises using MySQL, revealing a 43% increase in purchases of used vehicles.
+          <li> Monitored click-through conversion rate and traffic of AutoNation site over a 10 week period, yielding a 34% success rate.
+ </li> 
+          <li> Received training in Google Analytics for AutoNation’s web-based services, resulting in a Google Analytics Basic Certificate. </li> 
+ </li> 
           </ul>
 
       <h2> Relevant Coursework </h2>
       <h3> Florida State University </h3> 
       <ul> 
-        <li> CAP 5619: Deep and Reinforcement Learning [IN PROGRESS] </li>
-        <li> STA 5635: Machine Learning [IN PROGRESS] </li>
+        <li> IN PROGRESS </li>
+        <ul> <li> CAP 5619: Deep and Reinforcement Learning  </li>
+        
+        
+        <li> STA 5635: Machine Learning  </li>
+        <li> CIS 5370: Computer Security  </li>
+        <li> CIS 5390: Introduction to Data Visualization  </li></ul>
+        
+        <li> PREVIOUSLY COMPLETED </li>
+        <ul>
         <li> CAP 5769: Advanced Data Science </li>
-        <li> CIS 5370: Computer Security [IN PROGRESS] </li>
-        <li> CIS 5390: Introduction to Data Visualization [IN PROGRESS] </li>
+        
         <li> STA 5207: Applied Regression Methods </li>
         <li> CAP 5768: Introduction to Data Science </li>
+        </ul>
 
       </ul>
       <h3> UC San Diego </h3>
